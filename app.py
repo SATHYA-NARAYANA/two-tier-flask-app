741ce05a29ba549b40e70ac2b5498b46deef104a
Sathya Narayana <54190045+SATHYA-NARAYANA@users.noreply.github.com>
2023-11-22 17:37:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,46 +4,31 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# Configure MySQL database from environment variables
+# Configure MySQL from environment variables
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
 
-# Initialize MySQL connection
+# Initialize MySQL
 mysql = MySQL(app)
 
-# Route to display messages
 @app.route('/')
-def display_messages():
-    # Create a cursor to interact with the database
+def hello():
     cur = mysql.connection.cursor()
-    # Execute SQL query to fetch messages from the 'messages' table
     cur.execute('SELECT message FROM messages')
-    # Fetch all messages from the query result
     messages = cur.fetchall()
-    # Close the cursor
     cur.close()
-    # Render the 'index.html' template with the messages
     return render_template('index.html', messages=messages)
 
-# Route to submit new messages
 @app.route('/submit', methods=['POST'])
-def submit_message():
-    # Get the new message from the form
+def submit():
     new_message = request.form.get('new_message')
-    # Create a cursor to interact with the database
     cur = mysql.connection.cursor()
-    # Execute SQL query to insert the new message into the 'messages' table
     cur.execute('INSERT INTO messages (message) VALUES (%s)', [new_message])
-    # Commit the changes to the database
     mysql.connection.commit()
-    # Close the cursor
     cur.close()
-    # Redirect to the display_messages route after submission
-    return redirect(url_for('display_messages'))
+    return redirect(url_for('hello'))
 
-# Run the Flask application
 if __name__ == '__main__':
-    # Start the Flask app on specified host and port, enabling debugging
     app.run(host='0.0.0.0', port=5000, debug=True)
